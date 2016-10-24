3e5c2de263272b3a09e958a69abf4085969b27b8
Callum Jones <cj@icj.me>
2016-10-24 21:56:10 +0100

    add basic appointment information modal (read-only)

--- assets/js/modules/day-view.js
+++ assets/js/modules/day-view.js
@@ -3,7 +3,8 @@
 var $dayView;
 var $grid;
 var $appointments;
-
+var appointments;
+var $appointmentInfoModal;
 var calendarData = require("../model/calendar-data");
 
 module.exports = {
@@ -16,8 +17,27 @@ module.exports = {
 
         $grid         = $dayView.find(".day-view__grid");
         $appointments = $dayView.find(".day-view__appointments");
+        $appointmentInfoModal = $("#appointment-info");
 
         this.loadAppointments();
+
+        $(document).on("click", ".appointment", function() {
+            var searchId = $(this).attr("data-id");
+            var appointment;
+
+            console.log(searchId);
+            for (var appointmentIndex = 0; appointmentIndex < appointments.length; appointmentIndex++) {
+                if (appointments[appointmentIndex].id == searchId) {
+                    appointment = appointments[appointmentIndex];
+                    break;
+                }
+            }
+
+            console.log(appointment);
+
+            populateModal($appointmentInfoModal, appointment);
+            $appointmentInfoModal.modal("toggle");
+        });
     },
 
     loadAppointments: function() {
@@ -32,8 +52,10 @@ module.exports = {
                 return;
             }
 
-            for (var appointmentIndex = 0; appointmentIndex < response.data.length; appointmentIndex++) {
-                $appointments.append(createAppointment(response.data[appointmentIndex]));
+            appointments = response.data;
+
+            for (var appointmentIndex = 0; appointmentIndex < appointments.length; appointmentIndex++) {
+                $appointments.append(createAppointment(appointments[appointmentIndex]));
             }
         });
     }
@@ -55,4 +77,13 @@ function createAppointment(appointment) {
     $("<time>").text(startTime.format("HH:mm") + "-" + endTime.format("HH:mm")).appendTo($appointment);
 
     return $appointment;
+}
+
+function populateModal($modal, appointment) {
+    var startTime = moment().minute(0).hour(0).add(appointment.start, "minutes");
+    var endTime = startTime.clone().add(appointment["length"], "minutes");
+
+    $modal.find(".appointment-info__description").html(appointment.name);
+    $modal.find(".start-time").val(startTime.format("HH:mm:00"));
+    $modal.find(".finish-time").val(endTime.format("HH:mm:00"));
 }
