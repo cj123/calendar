646db61f372f5fbf4c2807481e083351940a6ff1
Callum Jones <cj@icj.me>
2016-11-06 16:36:48 +0000

    #22: remove scroll animation

--- frontend/src/js/directive/day-view.js
+++ frontend/src/js/directive/day-view.js
@@ -27,7 +27,7 @@ angular.module("calendar").directive("dayView", [function() {
                     CalendarOptions.get().then(function(response) {
                         var opts = response.data;
 
-                        angular.element(document.getElementById("day-view")).scrollTop(60 * opts.DayviewTimeStart, 1000);
+                        angular.element(document.getElementById("day-view")).scrollTop(60 * opts.DayviewTimeStart, 0);
                     });
                 });
             }
